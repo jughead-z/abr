497e81a18460974ec2dd425169eea01001d1c746
Criss <tahafoudali@gmail.com>
2023-11-04 19:01:44 +0100

    wa3

--- src/pages/alwaysbereal.jsx
+++ src/pages/alwaysbereal.jsx
@@ -1,21 +1,12 @@
 import Head from "next/head";
 import RootLayout from "@/components/common/layout/RootLayout";
-import CreativeAgencyHero from "@/components/hero/CreativeAgencyHero";
-import CreativeAgencyAbout from "@/components/about/CreativeAgencyAbout";
-import CreativeAgencyService from "@/components/service/CreativeAgencyService";
-import CreativeAgencyAward from "@/components/award/CreativeAgencyAward";
-import CreativeAgencyPortfolio from "@/components/portfolio/CreativeAgencyPortfolio";
-import CreativeAgencyTeam from "@/components/team/CreativeAgencyTeam";
-import CreativeAgencyBrand from "@/components/brand/CreativeAgencyBrand";
-import CreativeAgencyBlog from "@/components/blog/CreativeAgencyBlog";
-import CreativeAgencyCTA from "@/components/cta/CreativeAgencyCTA";
 import AbrAbout from "@/components/abr/AbrAbout";
-import AbrHero from "@/components/abr/abrHero";
-import AbrVideo from "@/components/abr/abrVideo";
-import AbrWork from "@/components/abr/abrWork";
-import AbrTeam from "@/components/abr/abrTeam";
-import AbrBlog from "@/components/abr/abrBlog";
-import AbrCta from "@/components/abr/abrCta";
+import AbrHero from "@/components/abr/AbrHero";
+import AbrVideo from "@/components/abr/AbrVideo";
+import AbrWork from "@/components/abr/AbrWork";
+import AbrTeam from "@/components/abr/AbrTeam";
+import AbrBlog from "@/components/abr/AbrBlog";
+import AbrCta from "@/components/abr/AbrCta";
 const Abr = () => {
   return (
     <div>
